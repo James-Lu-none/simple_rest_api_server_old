6da0bbb133ac4de86d7a89e05f87c42587fea67f
James-Lu-none <jamesabcde277@gmail.com>
2024-02-27 19:14:54 +0800

    add example post api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,7 +9,7 @@ admin.initializeApp({
 
 import express = require("express");
 import cors = require("cors");
-
+const db = admin.firestore();
 const app = express();
 app.use(cors({origin: true}));
 
@@ -17,4 +17,20 @@ app.get('/', (req, res) => {
    return res.status(200).send("Hi"); 
 });
 
+app.post("/api/create",(req,res)=>{
+    (async ()=>{
+        try{
+            await db.collection('userDetails').doc(`/${Date.now()}/`).create({
+                id:Date.now(),
+                name: req.body.name,
+                mobile: req.body.mobile,
+                address: req.body.address,
+            });
+            return res.status(200).send({status: "Success", msg: "Data Saved"});
+        }catch(error){
+            console.log(error);
+            return res.status(500).send({status: "Failed", msg: error});
+        }
+    })();
+});
 exports.app = functions.https.onRequest(app);
