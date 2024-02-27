a622f52d7c645b7d8f6f611bfb7d3c91507c5a64
James-Lu-none <jamesabcde277@gmail.com>
2024-02-27 21:41:52 +0800

    add set pc status method

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,6 +10,7 @@ admin.initializeApp({
 import express = require("express");
 import cors = require("cors");
 const db = admin.firestore();
+const pc_status_collection=db.collection('pc_status');
 const app = express();
 app.use(cors({origin: true}));
 
@@ -33,4 +34,35 @@ app.post("/api/create",(req,res)=>{
         }
     })();
 });
+
+app.put("/api/pc_status/set",(req,res)=>{
+    (async ()=>{
+        console.log(req.params);
+        console.log(req.body);
+        try{
+            var data = {};
+            var pcId = req.query.pc_id;
+            console.log(pcId);
+            if(pcId === null || typeof pcId !== 'string'){
+                throw Error("pc id was invalid");
+            }
+            data = assignBooleanIfNotNullOrUndefined('isPcOn', req.body.isPcOn, data);
+            data = assignBooleanIfNotNullOrUndefined('isPowerOnTriggered', req.body.isPowerOnTriggered, data);
+            console.log(data);
+            await pc_status_collection.doc(pcId).update(data);
+            return res.status(200).send({status: "Success", msg: "Data Saved"});
+        }catch(error){
+            console.log(error);
+            return res.status(500).send({status: "Failed", msg: error});
+        }
+    })();
+});
+
+function assignBooleanIfNotNullOrUndefined(property: string, value: any, data: {}) {
+    if (value !== null && value !== undefined && typeof value === 'boolean') {
+        data = Object.assign({ [property]: value }, data);
+    }
+    return data;
+}
+  
 exports.app = functions.https.onRequest(app);
